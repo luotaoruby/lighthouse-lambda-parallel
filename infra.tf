779778d9ddfbedd0c1e1a0c0b970c468ec59fe0c
luotao <luotao.ruby@gmail.com>
2019-07-24 15:27:20 +0800

    Bump app_version.

--- infra.tf
+++ infra.tf
@@ -1,6 +1,6 @@
 # Plug in your unique configuration here 
 locals {
-  app_version         = "0.0.95"
+  app_version         = "0.0.96"
   org                 = "lt"
   aws_region          = "us-west-2"
   aws_creds_file_path = "~/.aws/credentials"

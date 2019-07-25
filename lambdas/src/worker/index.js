6e44fc923e19ab038083614d35b252c9ad7b2788
luotao <luotao.ruby@gmail.com>
2019-07-25 18:49:49 +0800

    Bump app_version.

--- lambdas/src/worker/index.js
+++ lambdas/src/worker/index.js
@@ -162,7 +162,7 @@ exports.handler = async function(event, context) {
 
   console.log('JsonReport', jsonReport)
 
-  json = parseJsonReport(JSON.parse(jsonReport))
+  json = jsonReport
 
   const updatedRun = {
     TableName: process.env.RUNS_TABLE_NAME,
